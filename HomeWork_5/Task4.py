b394bd22b6a9b4be975f4f6c290d5b3c52e3c821
MariaKhristova <88403385+MariaKhristova@users.noreply.github.com>
2022-10-12 09:37:19 +0300

    Дз 5 задача 4

--- HomeWork_5/Task4.py
+++ HomeWork_5/Task4.py
@@ -1,1 +1,14 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+line = 'AAAAAAAABCCCCAAAAANNNN'
+rle_list = []
+
+for i in list(line):
+    if len(rle_list) == 0 or rle_list[-1][0] != i:
+        rle_list.append([i, 1])
+    else:
+        rle_list[-1][1] += 1
+
+rle_str = list(map(lambda x: f'{x[1]}x{x[0]}', rle_list))
+
+print(rle_str)
